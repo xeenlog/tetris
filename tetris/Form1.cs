0f373143c80385cfe2c91fdaaaae1a179b0531c7
lef <lef@DESKTOP-9SMBLI6>
2021-07-26 22:44:37 +0200

    setka

---
+++ tetris/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tetris
+{
+    public partial class Form1 : Form
+    {
+        TetrisGame tetris = new TetrisGame(20, 10, 20);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            tetris.Drow(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ tetris/TetrisGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris
+{
+    class TetrisGame
+    {
+        private int gameFieldHeightInCells;
+        private int gameFieldWidthInCells;
+        private int cellSize;
+
+        public TetrisGame(int gameFieldHeightInCells, int gameFieldWidthInCells, int cellSize)
+        {
+            this.gameFieldHeightInCells = gameFieldHeightInCells;
+            this.gameFieldWidthInCells = gameFieldWidthInCells;
+            this.cellSize = cellSize;
+        }
+
+        public void Drow(Graphics graphics)
+        {
+            for (int i = 0; i <= gameFieldHeightInCells; i++)
+            {
+                graphics.DrawLine(Pens.Black, 0, cellSize * i, cellSize * gameFieldWidthInCells, cellSize * i);
+            }
+
+            for (int j = 0; j <= gameFieldWidthInCells; j++)
+            {
+                graphics.DrawLine(Pens.Black, cellSize * j, 0, cellSize * j, cellSize * gameFieldHeightInCells);
+            }
+        }
+    }
+}
